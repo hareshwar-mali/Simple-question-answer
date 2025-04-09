ff35c2c37a58d5d76ada2f985f8081cfb58a7b5d
Hareshwar <hareshwarmali20@gmail.com>
2025-04-09 22:40:16 +0530

    register model with all fields

--- SimpleQnA/chat/admin.py
+++ SimpleQnA/chat/admin.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 from .models import Question, Answer
 
 
+@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'created_at')
-    search_fields = ('title', 'body', 'user__username')
+    list_display = ['id', 'user', 'title', 'body', 'created_at']
+    search_fields = ['title', 'body', 'user__username']
+    list_filter = ['created_at']
 
 
+@admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('question', 'user', 'created_at')
-    search_fields = ('body', 'user__username')
+    list_display = ['id', 'question', 'user', 'body', 'created_at', 'total_likes_display']
+    search_fields = ['body', 'user__username', 'question__title']
+    list_filter = ['created_at']
 
+    def total_likes_display(self, obj):
+        return obj.total_likes()
 
-admin.site.register(Question)
-admin.site.register(Answer)
+    total_likes_display.short_description = 'Total Likes'
